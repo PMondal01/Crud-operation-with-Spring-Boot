fc62206c12f674cc3e1afdaec8ed60efb3abf5d7
pratik.mondal <ABCD1234>
2020-09-05 11:59:30 +0600

    crud with spring boot

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,30 @@
+package com.example.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.sun.istack.internal.NotNull;
+
+import java.util.UUID;
+
+/**
+ * Created by pratik.mondal on 9/4/2020.
+ */
+public class Person {
+
+    private final UUID id;
+    @NotNull
+    private final String name;
+
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
